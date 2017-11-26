d6fce1bf61f75d99b75bdbdf9ab6701eac259f6d
Ghana Nazala <ghana.nazala@ui.ac.id>
2017-11-26 19:32:02 +0700

    2 Adding RFID Library

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,12 @@ Coded by Ghana Nazala, Athina Maria, Achmad Kripton, Muhammad Fadli
 
 #include <Arduino.h>
 #include <Wire.h>
+#include <SPI.h>
 #include "LiquidCrystal_I2C.h"
 #include "Keypad.h"
+#include "MFRC522.h"
 
+//Setup the pin for keypad
 const byte ROWS = 4; //four rows
 const byte COLS = 3; //three columns
 char keys[ROWS][COLS] = {
@@ -19,14 +22,19 @@ byte colPins[COLS] = {4, 3, 2}; //connect to the row pinouts of the keypad
 byte rowPins[ROWS] = {8, 7, 6, 5}; //connect to the column pinouts of the keypad
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
-// Set the LCD address to 0x27 for a 16 chars and 2 line display
+// Set init for the LCD with address 0x3F for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
+
+MFRC522 nfc(10, A6);
 
 void setup()
 {
+  //initialize the RFID
+  SPI.begin();
+  nfc.begin();
 	// initialize the LCD
 	lcd.begin();
-	lcd.print("Hello, there world!");
+	lcd.print("Hello, there!");
 }
 
 void loop()
@@ -36,6 +44,5 @@ void loop()
   if (key){
     lcd.print(key);
     delay(5000);
-    lcd.clear();
   }
 }
